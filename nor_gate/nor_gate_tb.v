97cef8c1f4dd5c62a882df28628ce42a331e07cd
Chokalingam166 <chokalingamamcet@gmail.com>
2025-08-08 14:30:54 +0530

    Create nor_gate_tb.v

---
+++ nor_gate/nor_gate_tb.v
@@ -0,0 +1,25 @@
+module nor_gate_tb;
+    reg A, B;
+    wire Y;
+
+    // Instantiate the NOR gate
+    nor_gate uut (
+        .A(A),
+        .B(B),
+        .Y(Y)
+    );
+
+    initial begin
+        // Test cases
+        A = 0; B = 0; #10;
+        A = 0; B = 1; #10;
+        A = 1; B = 0; #10;
+        A = 1; B = 1; #10;
+
+        $finish;
+    end
+
+    initial begin
+        $monitor("Time=%0t | A=%b B=%b | Y=%b", $time, A, B, Y);
+    end
+endmodule
